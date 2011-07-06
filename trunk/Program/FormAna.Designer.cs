a1d26a6bd42edc63d459e6cd36a2d23ca13231b4
mehmetakifkazan@gmail.com <mehmetakifkazan@gmail.com@4d09bc7b-f49f-c441-0d4a-67dad6a106c4>
2011-07-06 15:20:51 +0000

    görev 3 tamam.

--- trunk/Program/FormAna.Designer.cs
+++ trunk/Program/FormAna.Designer.cs
@@ -34,7 +34,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(151, 339);
+            this.button1.Location = new System.Drawing.Point(431, 339);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(131, 23);
             this.button1.TabIndex = 0;
@@ -44,7 +44,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(12, 339);
+            this.button2.Location = new System.Drawing.Point(292, 339);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(133, 23);
             this.button2.TabIndex = 1;
