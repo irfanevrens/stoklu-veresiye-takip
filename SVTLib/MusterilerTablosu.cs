fffad506a6b62acd9370af319350fe5c6b64e990
irfanevrens@gmail.com <irfanevrens@gmail.com@4d09bc7b-f49f-c441-0d4a-67dad6a106c4>
2011-07-15 15:51:34 +0000

    veresiye modülü planlandı müşteri listesi ana ekranda gösterilmeye başlandı

---
+++ SVTLib/MusterilerTablosu.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SQLite;
+using System.Collections;
+using System.Data;
+
+namespace SVTLib
+{
+    public class MusterilerTablosu
+    {
+        private string connString = Ayarlar.connString;
+
+        /// <summary>
+        /// Müşterilerin listesini almak için
+        /// </summary>
+        /// <returns></returns>
+        public ArrayList GetListe1()
+        {
+            SQLiteConnection conn = new SQLiteConnection();
+            conn.ConnectionString = connString;
+
+            string selectSQL =  
+"SELECT id, adi " + 
+"FROM " +
+  "musteriler " +
+"ORDER BY " + 
+  "adi ASC";
+
+            SQLiteCommand command = new SQLiteCommand(selectSQL, conn);
+
+            SQLiteDataReader reader;
+
+            ArrayList arrayList = new ArrayList();
+
+            try
+            {
+                conn.Open();
+
+                reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    arrayList.Add(new Musteri(reader.GetInt32(0), reader.GetString(1)));
+                }
+
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return arrayList;
+        }
+    }
+}
