b9e1120274b91c12394895a141627954bed39a4d
mehmetakifkazan@gmail.com <mehmetakifkazan@gmail.com@4d09bc7b-f49f-c441-0d4a-67dad6a106c4>
2011-07-15 19:04:45 +0000

    ufak tefek degişiklikler yapıldı

--- trunk/Program/FormAna.Designer.cs
+++ trunk/Program/FormAna.Designer.cs
@@ -43,10 +43,10 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(15, 407);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Location = new System.Drawing.Point(190, 367);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(175, 32);
+            this.button1.Size = new System.Drawing.Size(163, 32);
             this.button1.TabIndex = 0;
             this.button1.Text = "Yeni Ürün Ekle";
             this.button1.UseVisualStyleBackColor = true;
@@ -54,10 +54,10 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(13, 367);
-            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Location = new System.Drawing.Point(17, 367);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(177, 32);
+            this.button2.Size = new System.Drawing.Size(163, 32);
             this.button2.TabIndex = 1;
             this.button2.Text = "Ürün Listesi";
             this.button2.UseVisualStyleBackColor = true;
@@ -65,10 +65,10 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(220, 387);
-            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button3.Location = new System.Drawing.Point(362, 367);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(211, 32);
+            this.button3.Size = new System.Drawing.Size(163, 32);
             this.button3.TabIndex = 2;
             this.button3.Text = "Miktar Birimleri Listesi";
             this.button3.UseVisualStyleBackColor = true;
@@ -81,16 +81,16 @@
             this.dGWMusteriListesi.AllowUserToResizeColumns = false;
             this.dGWMusteriListesi.AllowUserToResizeRows = false;
             this.dGWMusteriListesi.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
-            this.dGWMusteriListesi.BackgroundColor = System.Drawing.Color.Moccasin;
+            this.dGWMusteriListesi.BackgroundColor = System.Drawing.SystemColors.GradientActiveCaption;
             this.dGWMusteriListesi.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dGWMusteriListesi.Location = new System.Drawing.Point(13, 53);
-            this.dGWMusteriListesi.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dGWMusteriListesi.Location = new System.Drawing.Point(17, 53);
+            this.dGWMusteriListesi.Margin = new System.Windows.Forms.Padding(4);
             this.dGWMusteriListesi.Name = "dGWMusteriListesi";
             this.dGWMusteriListesi.ReadOnly = true;
             this.dGWMusteriListesi.RowHeadersVisible = false;
             this.dGWMusteriListesi.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
             this.dGWMusteriListesi.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dGWMusteriListesi.Size = new System.Drawing.Size(514, 306);
+            this.dGWMusteriListesi.Size = new System.Drawing.Size(508, 306);
             this.dGWMusteriListesi.TabIndex = 3;
             // 
             // menuStrip1
@@ -116,18 +116,18 @@
             // yeniMüşteriToolStripMenuItem
             // 
             this.yeniMüşteriToolStripMenuItem.Name = "yeniMüşteriToolStripMenuItem";
-            this.yeniMüşteriToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.yeniMüşteriToolStripMenuItem.Size = new System.Drawing.Size(143, 22);
             this.yeniMüşteriToolStripMenuItem.Text = "Yeni Müşteri";
             // 
             // toolStripMenuItem1
             // 
             this.toolStripMenuItem1.Name = "toolStripMenuItem1";
-            this.toolStripMenuItem1.Size = new System.Drawing.Size(149, 6);
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(140, 6);
             // 
             // çıkışToolStripMenuItem
             // 
             this.çıkışToolStripMenuItem.Name = "çıkışToolStripMenuItem";
-            this.çıkışToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.çıkışToolStripMenuItem.Size = new System.Drawing.Size(143, 22);
             this.çıkışToolStripMenuItem.Text = "Çıkış";
             this.çıkışToolStripMenuItem.Click += new System.EventHandler(this.çıkışToolStripMenuItem_Click);
             // 
@@ -143,7 +143,7 @@
             this.Controls.Add(this.menuStrip1);
             this.Font = new System.Drawing.Font("Trebuchet MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.MainMenuStrip = this.menuStrip1;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "FormAna";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
