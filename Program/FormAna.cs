50dd6966448b97b44c048bb9e6b326870682f9a2
irfanevrens@gmail.com <irfanevrens@gmail.com@4d09bc7b-f49f-c441-0d4a-67dad6a106c4>
2011-07-18 18:42:42 +0000

    conn adında iki dosya oluşturulmuş onlar silindi FormAna.cs içerisindeki müşteri silme fonksiyonu için dökümantasyon çalışması yapıldı

--- Program/FormAna.cs
+++ Program/FormAna.cs
@@ -109,38 +109,120 @@ namespace Program
 
         private void bMusteriSil_Click(object sender, EventArgs e)
         {
+            // müşteri silme işlemi yapmadan evvel kullanıcıdan 
+            // yaptığı işin onayını almak için soru soruyoruz
             if (MessageBox.Show("Seçili müşteriler silinecek! Emin misiniz?", "Müşteriler Silinecek", MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.OK)
             {
-                MusterilerTablosu musterilerTablosu = new MusterilerTablosu();
-                VeresiyelerTablosu veresiyelerTablosu = new VeresiyelerTablosu();
-                TahsilatlarTablosu tahsilatlarTablosu = new TahsilatlarTablosu();
-                // @TODO tahsilatlar tablosu için sınıf yapılacak ve burada bir nesne oluşturulacak
+                // eğer karşısına çıkan uyarıya OK 
+                // derse o zaman bu kısım çalışacak
 
+                // müşteriler tablosu için yapılacak 
+                // işlemleri bu sınıf üzerinden yapacağız.
+                MusterilerTablosu musterilerTablosu = new MusterilerTablosu();
+
+                // müşteriye ait veresiye kayıtları için 
+                // gerekli işlemlerde bu nesneyi kullanacağız
+                VeresiyelerTablosu veresiyelerTablosu = new VeresiyelerTablosu();
+
+                // müşteriye ait tahsilat kayıtları için 
+                // gerekli işlemlerde bu nesneyi kullanacağız
+                TahsilatlarTablosu tahsilatlarTablosu = new TahsilatlarTablosu();
+
+                // bu değişken datagrid üzerindeki silinmek üzere 
+                // işaretlenen kaytların index değerini tutmak 
+                // için kullanılacak. şöyle ki, diyelim ki 
+                // datagrid içerisinde 5 tane müşteri var
+                // bu müşterilerden 1. 3. ve 4. sıradaki müşterileri 
+                // silmek isteyelim. silme işlemine önce 1. 
+                // müşteriden başlar ve veritabanından müşteriyi
+                // sildiğimiz gibi datagrid üzerindende silersek, 
+                // bir sonraki adımda, daha önce 3. sırada olan 
+                // müşteri 2. sıraya gelecektir. böyle olunca da 
+                // silme ile ilgili sıkıntı çıkacaktır. işte bu 
+                // nedenle müşteriler veritabanından silindikten sonra 
+                // datagrid üzerindeki müşteri index'i bu değişkene 
+                // koyuluyor ve tüm seçili müşteriler silindikten sonra
+                // silinen müşterilere ait index değerlerine göre 
+                // datagrid üzerinden ilgili kayıtlar kaldırılıyor
                 ArrayList silinecekIndexs = new ArrayList();
 
+                // listeden seçilen müşterilerin şayet borcu ya da 
+                // bakiyesi bulunuyorsa o halde müşteri silinemeyeceği 
+                // için bu işlemlerin en sonunda silinemyen müşteri sayısı 
+                // kullanıcıya bilgi olarak verilmelidir. işte bu amaçla
+                // bu değişken kullanılacak ve her müşteri silinememe 
+                // durumunda değeri 1 artırılacaktır.
+                int silinemeyenKayitSayisi = 0;
+
+                // müşteri listesinin bulunduğu datagrid üzerindeki 
+                // seçili kayıtlar üzerinde dönme işlemine başlıyoruz. 
+                // diyelim ki 1. 3. ve 4. kayıtlar seçili olsun ve 
+                // kullanıcı müşterileri sil butonuna tıklamış olsun
+                // bu durumda bu döngüye 3 kez girilecek demektir.
                 for (int i = 0; i < dGWMusteriListesi.SelectedRows.Count; i++)
                 {
+                    // diyelim ki 1. 3. ve 4. sıradaki kayıtlar seçilmiş olsunlar
+                    // birinci dögüde 1. sıradaki kayıt işleneceği için bu değişkene
+                    // 1. sıradaki müşterinin id bilgisi atanacaktır. bu şekilde 
+                    // müşterinin id bilgisine göre işlemler yapabileceğiz.
                     int aktifMusteriId = Convert.ToInt32(dGWMusteriListesi.SelectedRows[i].Cells[0].Value);
-                    string aktifMusteriAdi = dGWMusteriListesi.SelectedRows[i].Cells[1].Value.ToString();
 
+                    // bu kontrol işlemi aktif müşterinin borcunun olup 
+                    // olmadığını kontrol etmektedir.
                     if (musterilerTablosu.IsYokBorcu(aktifMusteriId))
                     {
-                        silinecekIndexs.Add(dGWMusteriListesi.SelectedRows[i].Index);
-                        tahsilatlarTablosu.SilWhereMusteriId(aktifMusteriId); 
-                        veresiyelerTablosu.SilWhereMusteriId(aktifMusteriId);
-                        // @TODO burada müşteriye ait tahsilatların silinmesi için gerekli kod yazılacak
+                        // müşterinin herhangi bir borcu yoksa yani tahsilatları 
+                        // ile ödemeleri birbirine eşit ise ya da her hangi bir 
+                        // işlemi bulunmuyorsa bu kısım çalışacaktır.
 
+                        // müşteri üzerinde işlem yapmadan önce seçili müşterinin
+                        // index bilgisi silinecek index'ler değişkenine atanıyor
+                        // daha sonra bu index'lere göre datagrid üzerinden kayıtlar 
+                        // silinecektir.
+                        silinecekIndexs.Add(dGWMusteriListesi.SelectedRows[i].Index);
+
+                        // bu kısımda müşteriye ait tahsilatlar silinmektedir.
+                        // şayet müşterinin tahsilatı hiç yoksa herhangi bir 
+                        // kayıt silinmeyecektir.
+                        tahsilatlarTablosu.SilWhereMusteriId(aktifMusteriId); 
+
+                        // bu kısımda ise müşteriye ait veresiye kayıtları
+                        // silinmektedir. veresiyesi hiç olmayan müşteri için 
+                        // herhangi bir işlem yapmayacaktır.
+                        veresiyelerTablosu.SilWhereMusteriId(aktifMusteriId);
+
+                        // bu kısımda ise müşterinin kendisi sistemden tamamen 
+                        // kaldırılmaktadır
                         musterilerTablosu.SilById(aktifMusteriId);
                     }
                     else
                     {
-                        MessageBox.Show(aktifMusteriAdi + " isimli müşterinin veresiyesi ya da bakiyesi olduğu için silinenemdi.", "Müşteri Silinemedi", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                        // müşterinin borcu ya da bakiyesi varsa bu kısım 
+                        // çalışacaktır. 
+
+                        // müşterinin borcu ya da bakiyesi olduğu için 
+                        // silinemeyen kayıt sayısını 1 artırıyoruz.
+                        // daha sonra bu bilgiyi kullanacağız.
+                        silinemeyenKayitSayisi++;
                     }
                 }
 
+                // seçili müşteri kayıtları veritabanından silindikten sonra
+                // form üzerinde yer alan datagrid üzerindeki kayıtların 
+                // silinmesi işlemine geçebiliriz.
                 for (int i = 0; i < silinecekIndexs.Count; i++)
                 {
+                    // silinen kayıt sayısı kadar bu döngü döner ve 
+                    // her döngüde ilgili datagrid satırını siler
                     dGWMusteriListesi.Rows.RemoveAt(Convert.ToInt32(silinecekIndexs[i]));
+                }
+
+                // eğer silinemeyen bir müşteri varsa ekrana kaç 
+                // adet silinemyen müşteri olduğu bilgisini veren 
+                // bir bilgi mesajı çıkartılacaktır.
+                if (silinemeyenKayitSayisi > 0) 
+                {
+                    MessageBox.Show(silinemeyenKayitSayisi.ToString() + " adet müşteri borcu ya da bakiyesi olduğu için silinemedi.", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 }
             }
         }
