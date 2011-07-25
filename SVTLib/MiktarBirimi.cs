91a9b6070e9db5e861f1ad9225471c67b8307c40
irfanevrens@gmail.com <irfanevrens@gmail.com@4d09bc7b-f49f-c441-0d4a-67dad6a106c4>
2011-07-25 13:18:14 +0000

    projeye devam edilecek, 21. revizyonda şaka yapıldı

---
+++ SVTLib/MiktarBirimi.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SVTLib
+{
+    public class MiktarBirimi
+    {
+        public const string ID = "id";
+        public const string AD = "ad";
+
+        private int id;
+        private string ad;
+
+        public MiktarBirimi()
+        { 
+            
+        }
+
+        public MiktarBirimi(int id, string ad)
+        {
+            this.id = id;
+            this.ad = ad;
+        }
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string Ad
+        {
+            get { return ad; }
+            set { ad = value.Trim(); }
+        }
+    }
+}
---
+++ SVTLib/Veresiye.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SVTLib
+{
+    public class Veresiye
+    {
+        private int id;
+        private string aciklama;
+        private float miktar;
+        private int musteriId;
+        private DateTime tarih;
+
+        public Veresiye(int id, string aciklama, DateTime tarih, float miktar)
+        {
+            this.id = id;
+            this.aciklama = aciklama;
+            this.tarih = tarih;
+            this.miktar = miktar;
+        }
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string Aciklama
+        {
+            get { return aciklama; }
+            set { aciklama = value; }
+        }
+
+        public float Miktar
+        {
+            get { return miktar; }
+            set { miktar = value; }
+        }
+
+        public int MusteriId
+        {
+            get { return musteriId; }
+            set { musteriId = value; }
+        }
+
+        public DateTime Tarih
+        {
+            get { return tarih; }
+            set { tarih = value; }
+        }
+    }
+}
