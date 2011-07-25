4d5e57bf46ecf007de289cb472f7fbfd8db0454f
irfanevrens@gmail.com <irfanevrens@gmail.com@4d09bc7b-f49f-c441-0d4a-67dad6a106c4>
2011-07-25 13:18:14 +0000

    projeye devam edilecek, 21. revizyonda şaka yapıldı

---
+++ trunk/SVTLib/Hareket.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SVTLib
+{
+    public class Hareket
+    {
+        private int id;
+        private string aciklama;
+        private decimal veresiyeMiktari;
+        private decimal tahsilatMiktari;
+        private DateTime tarih;
+
+        private HareketTurleri hareketTuru;
+
+        public Hareket(int id, string aciklama, DateTime tarih, decimal veresiyeMiktari, decimal tahsilatMiktari, HareketTurleri hareketTuru)
+        {
+            this.id = id;
+            this.aciklama = aciklama;
+            this.tarih = tarih;
+            this.veresiyeMiktari = veresiyeMiktari;
+            this.tahsilatMiktari = tahsilatMiktari;
+            this.hareketTuru = hareketTuru;
+        }
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string Aciklama
+        {
+            get { return aciklama; }
+            set { aciklama = value; }
+        }
+
+        public decimal VeresiyeMiktari
+        {
+            get { return veresiyeMiktari; }
+            set { veresiyeMiktari = value; }
+        }
+
+        public decimal TahsilatMiktari
+        {
+            get { return tahsilatMiktari; }
+            set { tahsilatMiktari = value; }
+        }
+
+        public DateTime Tarih
+        {
+            get { return tarih; }
+            set { tarih = value; }
+        }
+
+        public HareketTurleri HareketTuru
+        {
+            get { return hareketTuru; }
+            set { hareketTuru = value; }
+        }
+    }
+
+
+
+    public enum HareketTurleri
+    { 
+        Veresiye,
+        Tahsilat
+    }
+
+ 
+}
